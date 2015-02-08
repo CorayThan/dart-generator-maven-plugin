4f5052381db3e92b9a704b85022917c7c070790a
Nathan Westlake <nwestlake@catalystitservices.com>
2015-02-07 16:46:47 -0800

    Pretty close to complete, need to test it out

--- src/main/java/com/nathanwestlake/dartgenerator/generators/MethodGenerator.java
+++ src/main/java/com/nathanwestlake/dartgenerator/generators/MethodGenerator.java
@@ -8,9 +8,6 @@ import com.nathanwestlake.dartgenerator.models.DartType;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by CorayThan on 1/21/2015.
- */
 public class MethodGenerator {
 
 	public List<String> generateConstructor(DartClass dartClass) {
@@ -27,19 +24,18 @@ public class MethodGenerator {
 
 	public List<String> generateJsonConstructor(DartClass dartClass) {
 		List<String> fromJson = new ArrayList<>();
-		fromJson.add("  " + dartClass.getClassName() + ".fromJson(Map<String, dynamic> json, {Map models}){");
+		fromJson.add("  factory " + dartClass.getClassName() + ".fromJson(Map<String, dynamic> json, {Map models}){");
 		fromJson.add("    if (models == null) {");
 		fromJson.add("      models = {};");
 		fromJson.add("    }");
-		fromJson.add("    models[json['@id']] = this;");
-		fromJson.add("    this(");
+		fromJson.add("    " + dartClass.getClassName() + " toReturn = new " + dartClass.getClassName() + "(");
 		for (int x = 0; x < dartClass.getFields().size(); x++) {
 			DartField dartField = dartClass.getFields().get(x);
 			String name = dartField.name;
 			String line;
 			if (dartField.type == DartType.CLASS) {
 				line = "      " + name + ": models[json['" + name + "']] != null ? models[json['" + name
-						+ "']] : new " + dartField.getFieldTypeString() + ".fromJson(json['" + name + "'], models)";
+						+ "']] : new " + dartField.getFieldTypeString() + ".fromJson(json['" + name + "'], models: models)";
 				//new House(json['house'], models)
 			} else {
 				line = "      " + name + ": json['" + name + "']";
@@ -50,22 +46,44 @@ public class MethodGenerator {
 			fromJson.add(line);
 		}
 		fromJson.add("      );");
+		fromJson.add("    models[json['@id']] = toReturn;");
+		fromJson.add("    return toReturn;");
 		fromJson.add("  }");
 		return fromJson;
 	}
 
 	public List<String> generateToJson(DartClass dartClass) {
 		List<String> toJson = new ArrayList<>();
-		toJson.add("  Map<String, dynamic> toJson() => <String, dynamic>{");
+		toJson.add("  /**");
+		toJson.add("   * Returns either a Map<String, dynamic> if this is the first reference to this object");
+		toJson.add("   * that toJson is called in on this call, or an int if it is not the first");
+		toJson.add("   */");
+		toJson.add("  Map<String, dynamic> toJson({Map<dynamic, int> models}) {");
+		toJson.add("    if (models == null) {");
+		toJson.add("      //element mapping to 0 is the index for the next model");
+		toJson.add("      models = {0:1};");
+		toJson.add("    }");
+		toJson.add("    if (models.containsKey(this)) {");
+		toJson.add("      return models[this];");
+		toJson.add("    } else {");
+		toJson.add("      models[this] = models[0];");
+		toJson.add("      models[0] = models[0] + 1;");
+		toJson.add("      return <String, dynamic>{");
 		for (int x = 0; x < dartClass.getFields().size(); x++) {
-			String name = dartClass.getFields().get(x).name;
-			String line = "    \"" + name + "\": " + name;
+			DartField field = dartClass.getFields().get(x);
+			String name = field.name;
+			String line = "          \"" + name + "\": " + name;
+			if (field.type == DartType.CLASS) {
+				line += ".toJson(models: models)";
+			}
 			if (x != dartClass.getFields().size() - 1) {
 				line += ",";
 			}
 			toJson.add(line);
 		}
-		toJson.add("  };");
+		toJson.add("      };");
+		toJson.add("    }");
+		toJson.add("  }");
 		return toJson;
 	}
 }
